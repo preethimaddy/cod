e7de1e74aaf06966ffa54067b3f4fbe87349f07c
Preethi Madhavan <98338783+preethimaddy@users.noreply.github.com>
2024-03-17 07:46:09 +0530

    Image, Title, Author

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const BookList = ()=> {
+  return  <section>
+   <Book/>
+  </section>
+  
+}
+const Book = ()=> {
+  return  <article>
+     <Image/>
+<Title/>
+<Author/>
+  </article>
+  
+}
+
+const Image =()=><h2>Image Placeholder</h2>
+const Title =()=><h3>Book Title</h3>
+const Author =()=>{
+return <h4>Author</h4>}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<BookList/>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
