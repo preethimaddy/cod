c2b04c8928604c8a2b65e52d79700f92c2b5f8c6
Preethi Madhavan <98338783+preethimaddy@users.noreply.github.com>
2024-03-18 16:08:16 +0530

    props - multiple approach

--- src/index.js
+++ src/index.js
@@ -4,19 +4,33 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import './index.css'
 
-const img='https://m.media-amazon.com/images/I/71hwUY5ZmxL._SY466_.jpg';
-const title='Intresting Facts for curious Minds';
-const author='Jordhan Moore';
-
+const firstbook = {
+  img:'https://m.media-amazon.com/images/I/71hwUY5ZmxL._SY466_.jpg',
+  title:'Intresting Facts for curious Minds',
+  author:'Jordhan Moore'
+}
+const secondbook = {
+  img:'https://m.media-amazon.com/images/I/71sBtM3Yi5L._SY466_.jpg',
+  title:'The Power of Your Subconscious Mind',
+  author:'Joseph Murphy'
+}
+const thirdbook = {
+  img:'https://m.media-amazon.com/images/I/41n4z8Xv1BL._SY445_SX342_.jpg',
+  title:'Same As Ever',
+  author:'Morgan Housel'
+}
 const BookList = ()=> {
   return  <section className='booklist'>
-   <Book author={author} title={title} img={img}/>
-   <Book  author={author} title={title} img={img}/>
+   <Book author={firstbook.author} title={firstbook.title} img={firstbook.img}/>
+   <Book  author={secondbook.author} title={secondbook.title} img={secondbook.img}/>
+   <Book  author={thirdbook.author} title={thirdbook.title} img={thirdbook.img}/>
 
   </section>
 }
 const Book = (props)=> {
+  
   console.log(props);
+
   return  <article  className='book'>
      <img src={props.img} alt={props.title}/>
      <h2>{props.title}</h2>
