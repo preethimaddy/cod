e5f379c3825a41e4e8cbd73649be5ea4c1aa0fd8
Preethi Madhavan <98338783+preethimaddy@users.noreply.github.com>
2024-03-22 14:16:43 +0530

    keyprop with id

--- src/index.js
+++ src/index.js
@@ -8,18 +8,21 @@ const books =[
   {
     img:'https://m.media-amazon.com/images/I/71hwUY5ZmxL._SY466_.jpg',
     title:'Intresting Facts for curious Minds',
-    author:'Jordhan Moore'
+    author:'Jordhan Moore',
+    id:1
   },
    {
     img:'https://m.media-amazon.com/images/I/71sBtM3Yi5L._SY466_.jpg',
     title:'The Power of Your Subconscious Mind',
-    author:'Joseph Murphy'
+    author:'Joseph Murphy',
+    id:2
   }, 
 
   {
     img:'https://m.media-amazon.com/images/I/41n4z8Xv1BL._SY445_SX342_.jpg',
     title:'Same As Ever',
-    author:'Morgan Housel'
+    author:'Morgan Housel',
+    id:3
   }];
 
 //   const names = ['john', 'peter' ,'susan'];
@@ -32,9 +35,9 @@ const BookList =() => {
   return (
      <section className='booklist'>
  {books.map((book)=>{
- const {img, title, author} = book;
+ const {img, title, author, id} = book;
 
-return <Book img={img} title={title} author={author} />
+return <Book img={img} title={title} author={author} key={id}/>
   
  })}
     </section>
