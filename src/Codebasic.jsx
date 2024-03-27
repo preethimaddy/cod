3d5c82b57664187b7e35582700ec3d05530311cd
Preethi Madhavan <98338783+preethimaddy@users.noreply.github.com>
2024-03-27 21:38:22 +0530

    Multiple useEffects

--- src/Codebasic.jsx
+++ src/Codebasic.jsx
@@ -2,20 +2,25 @@ import React, { useEffect } from 'react'
 import {useState} from 'react'
 const Codebasic = () => {
     const [value, setvalue]= useState(0);
-
-    const sayHello =() => {
-console.log('Hello There!!');
-// Be careful you will have Infinite loop************************************************************8
-//If we updating state value inside the function_________ 
-    };
-    sayHello();
+    const [secondValue, setSecondValue] = useState(0);
+//     const sayHello =() => {
+// console.log('Hello There!!');
+// // Be careful you will have Infinite loop************************************************************8
+// //If we updating state value inside the function_________ 
+//     };
+//     sayHello();
     useEffect ( ()=>{
-        console.log('hello from useffect');
-    },[]);
+        console.log('hello from first useffect');
+    },[value]);
+    useEffect ( ()=>{
+        console.log('hello from second useffect');
+    },[secondValue]);
   return (
     <div>
         <h1> value: {value}</h1>
         <button className='btn' onClick={()=> setvalue(value+1)}>clickme</button>
+        <h1> SecondValue: {secondValue}</h1>
+        <button className='btn' onClick={()=> setSecondValue(secondValue+1)}>clickme</button>
     </div>
   )
 }
