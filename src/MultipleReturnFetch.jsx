f45cf5465bdf479dd30236f43932b239bd1cc71e
Preethi Madhavan <98338783+preethimaddy@users.noreply.github.com>
2024-03-28 20:38:38 +0530

    Fetch Data - Complete

--- src/MultipleReturnFetch.jsx
+++ src/MultipleReturnFetch.jsx
@@ -3,31 +3,38 @@ import { useState, useEffect } from 'react'
 
 const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturnFetch = () => {
-    const [user, setUser] = useState(null);
-    // const [isLoading, setIsLoading] = useState(true);
-    // const [isError, setIsError] = useState(null);
+     const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isError, setIsError] = useState(false);
 
     useEffect(()=>{
         const fetchUser = async ()=>{
 try{
     const resp = await fetch(url)
     const user = await resp.json();
-    console.log(user);
+    setUser(user);
+  
 }
 catch(error){
+    setIsError(true)
     console.log(error);
 }
+setIsLoading(false)
 };
   fetchUser()     
-    },[])
-    return <h3>fetchdata</h3>
-    // if(isLoading){
-    //     return<h2>Loading..</h2>
-    // }
-    // if(isError){
-    //     return<h2>Fetch Data</h2>
-    // }
-
+    },[]);
+    if(isLoading){
+        return<h2>Loading..</h2>
+    }
+    if(isError){
+        return<h2>There was an Error!!!</h2>
+    }
+ return <div>
+    <img style={{width:'150px', borderRadius: '25px'} }src={user.avatar_url} alt={user.name}/>
+    <h2>user.name</h2>
+    <h4>works at {user.company}</h4>
+    <p>{user.bio}</p>
+ </div>
 }
 
 export default MultipleReturnFetch
